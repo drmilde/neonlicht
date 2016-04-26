28bcc55421742143714cf7eb00b610194b720463
drmilde <milde@hs-fulda.de>
2016-04-26 21:56:23 +0200

    CosineGen, CosineTable and Wavetable added

--- doc/html/search/classes_1.js
+++ doc/html/search/classes_1.js
@@ -2,5 +2,7 @@ var searchData=
 [
   ['centralstore',['CentralStore',['../classCentralStore.html',1,'']]],
   ['configurationmanager',['ConfigurationManager',['../classConfigurationManager.html',1,'']]],
-  ['consumerthread',['ConsumerThread',['../classConsumerThread.html',1,'']]]
+  ['consumerthread',['ConsumerThread',['../classConsumerThread.html',1,'']]],
+  ['cosinegen',['CosineGen',['../classunit_1_1CosineGen.html',1,'unit']]],
+  ['cosinetable',['CosineTable',['../classCosineTable.html',1,'']]]
 ];
