98c310f4aaac9ca9c5fdcf09ef76779777d3904f
milde <milde@hs-fulda.de>
2016-05-13 11:05:45 +0200

    first commit on helix

--- doc/html/search/all_3.js
+++ doc/html/search/all_3.js
@@ -2,8 +2,8 @@ var searchData=
 [
   ['gatedconstantgen',['GatedConstantGen',['../classunit_1_1GatedConstantGen.html',1,'unit']]],
   ['genericqueue',['GenericQueue',['../classGenericQueue.html',1,'']]],
-  ['genericqueue_3c_20osc_3a_3amessagedata_20_2a_3e',['GenericQueue&lt; osc::MessageData *&gt;',['../classGenericQueue.html',1,'']]],
-  ['genericqueue_3c_20workitem_20_2a_3e',['GenericQueue&lt; WorkItem *&gt;',['../classGenericQueue.html',1,'']]],
+  ['genericqueue_3c_20osc_3a_3amessagedata_20_2a_20_3e',['GenericQueue&lt; osc::MessageData * &gt;',['../classGenericQueue.html',1,'']]],
+  ['genericqueue_3c_20workitem_20_2a_20_3e',['GenericQueue&lt; WorkItem * &gt;',['../classGenericQueue.html',1,'']]],
   ['genericthread',['GenericThread',['../classGenericThread.html',1,'']]],
   ['gui',['GUI',['../classGUI.html',1,'']]],
   ['guimapping',['GUIMapping',['../classGUIMapping.html',1,'']]]
